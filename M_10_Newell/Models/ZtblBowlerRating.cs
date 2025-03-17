6703e82726e0d385c9d2900a64b90373ea6e57d8
Dauce <126361698+Dauce@users.noreply.github.com>
2025-03-16 20:23:50 -0600

    Front end

---
+++ M_10_Newell/Models/ZtblBowlerRating.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace M_10_Newell.Models;
+
+public partial class ZtblBowlerRating
+{
+    public string BowlerRating { get; set; } = null!;
+
+    public short? BowlerLowAvg { get; set; }
+
+    public short? BowlerHighAvg { get; set; }
+}
